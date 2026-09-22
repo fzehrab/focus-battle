abc2382b535e5872d9010c1ff1db5fd01dd5ba09
fzehrab <fatmazehrabozdag@gmail.com>
2026-09-22 11:25:15 -0400

    Add Firebase configuration settings

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,9 @@
-Unsupported Media Type
+export const firebaseConfig = {
+  apiKey: "AIzaSyCPd4gg1iPwhCLxQ3ybrHjCuSAC2AmWb_w",
+  authDomain: "focus-battle-10141.firebaseapp.com",
+  databaseURL: "https://focus-battle-10141-default-rtdb.firebaseio.com",
+  projectId: "focus-battle-10141",
+  storageBucket: "focus-battle-10141.firebasestorage.app",
+  messagingSenderId: "695441310909",
+  appId: "1:695441310909:web:8374f52a79163c0545e3d4"
+};
